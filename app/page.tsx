622e3a642e6aa6b1d5f4c3a8d043c4d6a584d585
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-07-30 22:30:04 +0200

    Create basic formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <></>;
+  const state = "pomodoro";
+
+  return (
+    <div
+      className="app-container"
+      style={{ "--state": state } as React.CSSProperties}
+    >
+      <div className="app">
+        <time>14:53</time>
+        <input type="range" min="0" max="100" step="25" />
+      </div>
+    </div>
+  );
 }
